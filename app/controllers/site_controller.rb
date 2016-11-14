e665dc188d39baac1b41ebb1efb544fde37096fa
Alvarojoered <arojaspen@gmail.com>
2016-11-14 11:12:45 +0100

    Add calculator and params inputs

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -4,4 +4,14 @@ class SiteController < ApplicationController
 
 	def contact	
 	end
+
+	def calculator
+	end
+
+	def calculate
+		@num1 = params[:first_num].to_i
+		@num2 = params[:second_num].to_i
+		@result = @num1 + @num2
+		render 'calculate'
+	end
 end
