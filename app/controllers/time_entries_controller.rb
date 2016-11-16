f304978c1312f427b5a850e4b4ad6060fd81e6ea
Alvarojoered <arojaspen@gmail.com>
2016-11-16 13:08:38 +0100

    Add create form for time entry. Pending create method in Time_entry controller

--- app/controllers/time_entries_controller.rb
+++ app/controllers/time_entries_controller.rb
@@ -3,4 +3,10 @@ class TimeEntriesController < ApplicationController
 		@project = Project.find(params[:project_id])
 		@entries = @project.time_entries.where('created_at > ?', 1.month.ago.end_of_month)
 	end
+
+	def new
+		@project = Project.find(params[:project_id])
+		@entry = @project.time_entries.new
+	end
+
 end
